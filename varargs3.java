c746468add29029a14fb3f64cef373752c405cad
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2023-02-16 00:54:24 +0530

    Add files via upload

--- varargs3.java
+++ varargs3.java
@@ -27,10 +27,20 @@ public class varargs3 {
         System.out.println();
     }
 
+    static void vaTest(Float i, int... v) {
+        System.out.print("vaTest(int, int ...): " + "Number of args: " + v.length + " Contents: ");
+
+        for (int x : v){
+            System.out.print(x + " ");
+        }
+        System.out.println();
+    }
+
     public static void main(String args[]) {
         vaTest(1, 2, 3);
         vaTest("Testing: ", 10, 20);
         vaTest(true, false, false);
+        vaTest(1.09f, 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16);
     }
     
 }
